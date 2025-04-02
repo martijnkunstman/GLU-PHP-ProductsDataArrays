83efdc31f6bf115aec2df7f1916cda7680989515
Martijn Kunstman <martijn@kunstman.nl>
2025-04-02 15:11:35 +0200

    video 3

--- plaatsinwinkelwagen.php
+++ plaatsinwinkelwagen.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    echo($_GET['id']);
+    // echo($_GET['id']);
     $_SESSION["producten_ids"] = $_SESSION["producten_ids"] . "-" . $_GET['id'];
     header('Location: start.php');
 ?>
